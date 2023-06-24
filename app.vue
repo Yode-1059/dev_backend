fc3b97153540aab52ee1aeb6354ed7f080721f8b
Yode-1059 <yoshinaga.web@gmail.com>
2023-06-24 18:09:04 +0900

    little_fix

--- app.vue
+++ app.vue
@@ -3,3 +3,27 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+const { logout, login, user } = useUserState();
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+let auth: any;
+
+onMounted(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (authUser) => {
+    console.log(authUser, 'auth');
+
+    if (authUser) {
+      const loginUser: any = {
+        uid: authUser.uid,
+        displayName: authUser.displayName,
+      };
+      login(loginUser);
+    } else {
+      logout();
+    }
+  });
+});
+</script>
--- pages/profile.vue
+++ pages/profile.vue
@@ -6,33 +6,33 @@ const { logout, login, user } = useUserState();
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 let auth: any;
 
-onMounted(() => {
-  auth = getAuth();
-  onAuthStateChanged(auth, (authUser) => {
-    console.log(authUser, 'auth');
+// onMounted(() => {
+//   auth = getAuth();
+//   onAuthStateChanged(auth, (authUser) => {
+//     console.log(authUser, 'auth');
 
-    if (authUser) {
-      const loginUser: any = {
-        uid: authUser.uid,
-        displayName: authUser.displayName,
-      };
-      console.log("login")
-      login(loginUser);
-      onSnapshot(collection(db, 'users'), (querySnapshot: any) => {
-        querySnapshot.forEach((doc: any) => {
-          const likes: any = {
-            like: doc.data().likes
-          }
-          console.log(likes)
-        })
-      })
-    } else {
-      console.log("logout")
-      this.$router.push("index")
-      logout();
-    }
-  });
-});
+//     if (authUser) {
+//       const loginUser: any = {
+//         uid: authUser.uid,
+//         displayName: authUser.displayName,
+//       };
+//       console.log("login")
+//       login(loginUser);
+//       onSnapshot(collection(db, 'users'), (querySnapshot: any) => {
+//         querySnapshot.forEach((doc: any) => {
+//           const likes: any = {
+//             like: doc.data().likes
+//           }
+//           console.log(likes)
+//         })
+//       })
+//     } else {
+//       console.log("logout")
+//       this.$router.push("index")
+//       logout();
+//     }
+//   });
+// });
 
  const  getLike =async () => {
   console.log(user.value.uid)
