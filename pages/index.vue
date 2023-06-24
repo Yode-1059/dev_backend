df3128e0d318e06159a5a67d980b36f80d710975
Yode-1059 <yoshinaga.web@gmail.com>
2023-06-24 18:19:41 +0900

    hot_fix

--- pages/index.vue
+++ pages/index.vue
@@ -10,27 +10,27 @@
 
 <script setup lang="ts">
 const { logout, login, user } = useUserState();
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+// import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-import Auth from './auth.vue'
-import Post from './post.vue';
+// import Auth from './auth.vue'
+// import Post from './post.vue';
 
-let auth: any;
+// let auth: any;
 
-onMounted(() => {
-  auth = getAuth();
-  onAuthStateChanged(auth, (authUser) => {
-    console.log(authUser, 'auth');
+// onMounted(() => {
+//   auth = getAuth();
+//   onAuthStateChanged(auth, (authUser) => {
+//     console.log(authUser, 'auth');
 
-    if (authUser) {
-      const loginUser: any = {
-        uid: authUser.uid,
-        displayName: authUser.displayName,
-      };
-      login(loginUser);
-    } else {
-      logout();
-    }
-  });
-});
+//     if (authUser) {
+//       const loginUser: any = {
+//         uid: authUser.uid,
+//         displayName: authUser.displayName,
+//       };
+//       login(loginUser);
+//     } else {
+//       logout();
+//     }
+//   });
+// });
 </script>
